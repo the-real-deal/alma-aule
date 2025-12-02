d7278f623060afde322ad9819b2d9fed2a8f0790
Samuele Trapani <samuele.trapani@studio.unibo.it>
2025-12-02 14:40:27 +0100

    added index.html

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/alma-aule/');
+	header('Location: '.$uri.'/');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
