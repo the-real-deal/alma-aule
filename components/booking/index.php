f121e0f357831ef37767c318930dc0db9419e59f
Samuele Trapani <samuele.trapani@studio.unibo.it>
2026-01-17 15:25:07 +0100

    new page booking, made map as a common component

---
+++ components/booking/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require "{$_SERVER['DOCUMENT_ROOT']}/components/common/head.php" ?>
+    <title>Booking - Alma Aule</title>
+    <?php require "{$_SERVER['DOCUMENT_ROOT']}/components/common/map.php" ?>
+</head>
+
+<body>
+
+</body>
+
+</html>
--- components/home/index.php
+++ components/home/index.php
@@ -3,14 +3,13 @@
 
 <head>
     <?php require "{$_SERVER['DOCUMENT_ROOT']}/components/common/head.php" ?>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <title>Alma Aule</title>
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-    <script src="map.js" defer></script>
+
+    <title>Home - Alma Aule</title>
+
+
     <script src="prenotazioni.js"></script>
     <link href="./home.css" rel="stylesheet">
+    <?php require "{$_SERVER['DOCUMENT_ROOT']}/components/common/map.php" ?>
 </head>
 
 <body>
@@ -27,14 +26,14 @@
                     <h3 class="ps-3 m-0 text-uppercase">
                         Alma<br />Aule
                     </h3>
-                </a>     
+                </a>
                 <div class="collapse navbar-collapse text-white" id="navbarNav">
                     <div class="navbar-nav gap-1 gap-md-4">
                         <a class="nav-link text-white d-flex flex-row flex-md-column align-items-start align-items-md-center">
                             <strong class="fs-3 bi bi-house-fill"></strong>
                             <span class="d-inline-block my-auto ps-2 font-merriweather">Home</span>
                         </a>
-                        <a class="nav-link text-white d-flex flex-row flex-md-column align-items-start align-items-md-center">
+                        <a href="<?php echo "/components/booking" ?>" class="nav-link text-white d-flex flex-row flex-md-column align-items-start align-items-md-center">
                             <strong class="fs-3 bi bi-door-closed-fill"></strong>
                             <span class="d-inline-block my-auto ps-2 font-merriweather">Aule</span>
                         </a>
@@ -47,13 +46,13 @@
                             <span class="d-inline-block my-auto ps-2 font-merriweather">Profilo</span>
                         </a>
                     </div>
-                </div> 
+                </div>
             </div>
         </nav>
     </header>
     <main class="p-4">
         <div id="map"></div>
-        
+
         <section class="container mt-4">
             <div class="d-flex justify-content-between align-items-center mb-4">
                 <h2 class="text-uppercase mb-0">Le Tue Prenotazioni</h2>
