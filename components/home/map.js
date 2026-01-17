6b23ecc4b7d94e20d4d6a4243df1538a723ed32c
Samuele Trapani <samuele.trapani@studio.unibo.it>
2026-01-17 14:47:25 +0100

    map maxbound and marker color setted

--- components/home/map.js
+++ components/home/map.js
@@ -2,23 +2,37 @@ let mapOptions = {
     center: [44.494887, 11.342616],
     zoom: 16
 }
+const emiliaRomagnaBounds = [
+    [43.85, 10.5],   // Sud-Ovest (latitudine, longitudine)
+    [45.1, 13.3]     // Nord-Est (latitudine, longitudine)
+];
 
-let map = L.map('map', mapOptions).setView([44.494887, 11.342616], 13);
+let map = L.map('map', mapOptions).setView([44.494887, 11.342616], 13).setMaxBounds(emiliaRomagnaBounds);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; OpenStreetMap'
+    minZoom: 10,
+    attribution: '&copy; OpenStreetMap',
 }).addTo(map);
+map.fitBounds(emiliaRomagnaBounds)
 
+var redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 async function caricaSedi() {
     try {
         const response = await fetch('/apis/map.php');
-        
+
         const sedi = await response.json();
 
         sedi.forEach(sede => {
             if (sede.Via && sede.Latitudine && sede.Longitudine) {
-                
-                var marker = L.marker([sede.Latitudine, sede.Longitudine]).addTo(map);
+
+                var marker = L.marker([sede.Latitudine, sede.Longitudine], { icon: redIcon }).addTo(map);
                 marker.bindPopup(`<b>${sede.Via}</b>`);
             }
         });
@@ -26,6 +40,7 @@ async function caricaSedi() {
     } catch (error) {
         console.error("Errore nel caricamento delle sedi:", error);
     }
+
 }
 
 caricaSedi();
