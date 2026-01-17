9de341688f6e40c0a07134189d5b6d6ad36dd94a
Nickolausen <nicholas.magi24@gmail.com>
2026-01-17 14:49:07 +0100

    WIP Navbar

--- components/landing/index.php
+++ components/landing/index.php
@@ -8,10 +8,11 @@
     <script src="../common/animatedBackground.js"></script>
     <title>Alma Aule — Landing page</title>
 </head>
-<body class="d-flex justify-content-center align-items-center">
+<body class="d-flex justify-content-center align-items-center overflow-y-hidden">
     <main class="text-center text-white">
+        <img src="../../assets/imgs/logo-unibo.png">
         <h1 class="display-1 fw-bold text-uppercase">Alma Aule</h1>
-        <div class="fs-3">Prenota un'aula o una sala studio in pochi click.</div>
+        <div class="fs-3 text-wrap font-merriweather">Prenota un'aula o una sala studio <br class="d-md-none"/> in pochi click.</div>
         <button class="btn btn-primary mt-4">
             <a class="text-white text-decoration-none h-100 w-100" href="../login">
                 Accedi al servizio
