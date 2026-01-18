e7736576ea1afb011aafa74e7c4ae50f1a367ba6
Samuele Trapani <samuele.trapani@studio.unibo.it>
2026-01-18 13:17:23 +0100

    oh no I'm working on main branch, '**$%%G!'

--- apis/aule.php
+++ apis/aule.php
@@ -2,7 +2,8 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php';
 
 $data = array();
-foreach ($dbh->getSedi()->fetch_assoc() as $sede) {
-    $data += [$sede => $dbh->getAulePerSede($sede["CodiceSede"])];
+$sedi = $dbh->getSedi()->fetch_assoc();
+foreach ($sedi as $s) {
+    $data += [$s => $dbh->getAulePerSede($s["CodiceSede"])->fetch_array()];
 }
-echo $data;
+echo json_encode($data);
--- components/booking/booking.js
+++ components/booking/booking.js
@@ -1,4 +1,31 @@
 
 async function loadAule(params) {
-    const response = await fetch('/apis/aule.php');
+    const container = document.getElementsByClassName("card-body")
+    try {
+        const response = await fetch('/apis/aule.php');
+        if (!response.ok) {
+            throw new Error(`Errore HTTP: ${response.status}`);
+        }
+        const header = response.headers.get('content-type');
+        const text = await response.text;
+
+        let data;
+        try {
+            data = JSON.parse(text);
+        } catch (error) {
+            throw new Error('Risposta non valida dal server');
+        }
+        // if (data.error) {
+        //     container.innerHTML = mostraErrore(data.message);
+        //     return;
+        // }
+
+
+    } catch (error) {
+
+    }
+}
+
+function aule(sede) {
+
 }
