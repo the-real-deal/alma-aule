0cf4b691981b0818f6aac806c24920bfaf2244c5
Nickolausen <nicholas.magi24@gmail.com>
2026-01-18 14:50:02 +0100

    working wow effect

--- components/common/head.php
+++ components/common/head.php
@@ -1,6 +1,7 @@
 <?php 
     require_once "{$_SERVER['DOCUMENT_ROOT']}/bootstrap.php";
     define("COMMON_DIR", "../common");
+    define("ASSETS_DIR", "../../assets");
 ?>
 
 <meta charset="UTF-8">
@@ -11,10 +12,10 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 <!-- Global CSS (custom) -->
 <link rel="stylesheet" href="<?php echo COMMON_DIR . '/global.css'; ?>">
+<!-- JQuery -->
+<script src="<?php echo ASSETS_DIR . '/js/jquery-3.7.1-min.js' ?>"></script>
 <!-- Bootstrap Bundle (JS) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
-<!-- JQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" defer></script>
 
 <!-- Navbar -->
 <link rel="stylesheet" href="<?php echo COMMON_DIR . '/navbar/navbar.css'; ?>">
