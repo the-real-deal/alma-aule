2698683380ba4d9b6960a8e1372835544e584d2c
Nickolausen <nicholas.magi24@gmail.com>
2026-01-18 18:02:10 +0100

    fixed 404

--- components/404/index.php
+++ components/404/index.php
@@ -9,8 +9,8 @@
 <body>
     <main class="flex-grow-1 d-flex flex-column align-items-center justify-content-center text-white">
         <code class="badge text-bg-primary fs-5 mb-3">404 — Not Found</code>
-        <h1 class="display-1 text-uppercase text-primary">Oh no!</h1>
-        <p class="text-primary">Sembra che hai cercato una pagina inesistente.</p>
+        <h1 class="position-relative display-1 text-uppercase text-primary">Oh no!</h1>
+        <p class="text-dark">Sembra che hai cercato una pagina inesistente.</p>
         <a class="btn btn-primary" href="/home/">
             Torna alla home
         </a>
